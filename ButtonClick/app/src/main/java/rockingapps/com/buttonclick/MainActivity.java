67f6b25a46c61da3c848e130e307923e747b8820
sateesh kumar reddy <sateesh57@gmail.com>
2019-09-07 10:25:45 +0530

    sap

---
+++ ButtonClick/app/src/main/java/rockingapps/com/buttonclick/MainActivity.java
@@ -0,0 +1,27 @@
+package rockingapps.com.buttonclick;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button)findViewById(R.id.buttonHello);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this,"you click hello button..",Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
